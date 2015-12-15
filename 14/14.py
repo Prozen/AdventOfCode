279c655913114a8a700b317104bf40e282456efd
Pavel Rozenblioum <pavel.rozenblioum@jayway.com>
2015-12-15 19:26:46 +0700

    day 14 fix

--- 14/14.py
+++ 14/14.py
@@ -13,8 +13,6 @@ Vixen can fly 18 km/s for 5 seconds, but then must rest for 84 seconds.""".split
 reindeers =  [(x[0], int(x[1]), int(x[2]), int(x[3])) for x in [re.split(r" can fly | km/s for | seconds, but then must rest for | seconds\.", x) for x in data]]
 def reindistance(time, deer):
     return (time//(deer[3]+ deer[2])*deer[2] +min(time%(deer[3]+ deer[2]), deer[2]))*deer[1]
-#print(max([reindistance(2503,x) for x in reindeers]))
+print(max([reindistance(2503,x) for x in reindeers]))
 
-#dists = sorted([(reindistance(2503,x),x[0]) for x in reindeers])
-#print([x[1] for x in dists if x[0]== dists[-1][0]])
 print(Counter(itertools.chain(*[[x[1] for x in sorted([(reindistance(time,x),x[0]) for x in reindeers]) if x[0]== sorted([(reindistance(time,x),x[0]) for x in reindeers])[-1][0]] for time in range(1,2504)])))
